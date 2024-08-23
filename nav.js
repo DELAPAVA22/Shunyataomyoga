cebcc6be3526a4f573ef41b65d8c6913f7838f2b
juan <delapavajuanjose@gmail.com>
2024-08-23 15:57:47 -0500

    first commit

---
+++ nav.js
@@ -0,0 +1,7 @@
+document.getElementById("btn-ser").addEventListener("click", () => {
+  document.getElementById("servicios").scrollIntoView({ behavior: "smooth" });
+});
+
+document.getElementById("btn-con").addEventListener("click", () => {
+  document.getElementById("contactos").scrollIntoView({ behavior: "smooth" });
+});
